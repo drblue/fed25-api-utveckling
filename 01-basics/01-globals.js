9687da4a7cab769ac4733de617cd49d8d276cea0
Johan Nordström <johan@digitalvillage.se>
2025-12-22 10:26:30 +0100

    Global-object in Node.js.

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,24 @@
+/**
+ * Globals
+ */
+
+console.log("Hello backend from 01-globals.js");
+
+/*
+setTimeout(() => {
+	console.log("I'll be back!");
+}, 3000);
+*/
+
+/*
+setInterval(() => {
+	console.log("Hello?");
+}, 2000);
+*/
+
+console.log("Cheerio!");
+
+// globalThis.setTimeout(() => console.log("Hello from globalThis"), 1000);
+
+console.log("Absolute path to directory for this file:", __dirname);
+console.log("Absolute path to this file:", __filename);

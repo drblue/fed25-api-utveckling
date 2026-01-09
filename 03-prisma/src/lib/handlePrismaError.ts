b2cbb2293d740e20a577fe07b473226b469cabd4
Johan Nordström <johan@digitalvillage.se>
2026-01-09 10:54:59 +0100

    Handle Prisma validation errors.

--- 03-prisma/src/lib/handlePrismaError.ts
+++ 03-prisma/src/lib/handlePrismaError.ts
@@ -8,11 +8,20 @@ export const handlePrismaError = (res: Response, err: unknown) => {
 	if (err instanceof Prisma.PrismaClientKnownRequestError) {
 		// Was it not found?
 		if (err.code === "P2025") {
+			console.error(err);
 			res.status(404).send({ message: "Resource not found" });
 			return;
 		}
 	}
 
+	// Prisma validation error
+	if (err instanceof Prisma.PrismaClientValidationError) {
+		console.error(err);
+		res.status(400).send({ message: "Invalid request data" });
+		return;
+	}
+
+	// Fallback
 	console.error(err);
 	res.status(500).send({ message: "Something went wrong when querying the database" });
 }

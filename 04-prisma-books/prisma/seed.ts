25b812fe9b8919996cf9d8ea76cda3bd13f8a7a5
Johan Nordström <johan@digitalvillage.se>
2026-01-15 09:21:39 +0100

    Extended Prisma database seed example to include all resources.

--- 04-prisma-books/prisma/seed.ts
+++ 04-prisma-books/prisma/seed.ts
@@ -6,24 +6,200 @@ const main = async () => {
 	/**
 	 * 👩‍💼 Publishers
 	 */
+	const hutchinson = await prisma.publisher.upsert({
+		where: { id: 1 },
+		update: {},
+		create: { id: 1, name: "Hutchinson" },
+	});
 
-	/**
-	 * 📚 Books
-	 */
+	const gnome = await prisma.publisher.upsert({
+		where: { id: 2 },
+		update: {},
+		create: { name: "Gnome Press" },
+	});
+
+	const podium = await prisma.publisher.upsert({
+		where: { id: 3 },
+		update: {},
+		create: { name: "Podium Audio" },
+	});
+
+	const modern_library = await prisma.publisher.upsert({
+		where: { id: 4 },
+		update: {},
+		create: { name: "Modern Library" },
+	});
+
+	const del_rey = await prisma.publisher.upsert({
+		where: { id: 5 },
+		update: {},
+		create: { name: "Del Rey" },
+	});
+
+	const allen_and_unwin = await prisma.publisher.upsert({
+		where: { id: 6 },
+		update: {},
+		create: { name: "Allen & Unwin" },
+	});
 
 	/**
 	 * ✍🏻 Authors
 	 */
-	const astridLindgren = await prisma.author.upsert({
+	const clarke = await prisma.author.upsert({
 		where: { id: 1 },
 		update: {},
-		create: { id: 1, name: "Astrid Lindgren", /* books: { connect: { id: 3 } } */ },
+		create: { id: 1, name: "Sir Arthur C. Clarke", birthyear: 1917 },
 	});
 
-	const selmaLagerlof = await prisma.author.upsert({
+	const asimov = await prisma.author.upsert({
 		where: { id: 2 },
 		update: {},
-		create: { id: 2, name: "Selma Lagerlöf" },
+		create: { id: 2, name: "Isaac Asimov", birthyear: 1920 },
+	});
+
+	const anspach = await prisma.author.upsert({
+		where: { id: 3 },
+		update: {},
+		create: { id: 3, name: "Jason Anspach" },
+	});
+
+	const cole = await prisma.author.upsert({
+		where: { id: 4 },
+		update: {},
+		create: { id: 4, name: "Nick Cole" },
+	});
+
+	const tolkien = await prisma.author.upsert({
+		where: { id: 5 },
+		update: {},
+		create: { id: 5, name: "J. R. R. Tolkien", birthyear: 1892 },
+	});
+
+	const austen = await prisma.author.upsert({
+		where: { id: 6 },
+		update: {},
+		create: { id: 6, name: "Jane Austen", birthyear: 1775 },
+	});
+
+	const adams = await prisma.author.upsert({
+		where: { id: 7 },
+		update: {},
+		create: { id: 7, name: "Douglas Adams", birthyear: 1952 },
+	});
+
+	/**
+	 * 📚 Books
+	 */
+	await prisma.book.upsert({
+		where: { id: 1 },
+		update: {},
+		create: {
+			id: 1,
+			title: "2001: A Space Odessey",
+			pages: 224,
+			publisherId: hutchinson.id,
+			authors: {
+				connect: [ clarke ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 2 },
+		update: {},
+		create: {
+			id: 2,
+			title: "2010: Odessey Two",
+			pages: 291,
+			publisherId: hutchinson.id,
+			authors: {
+				connect: [ clarke ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 3 },
+		update: {},
+		create: {
+			id: 3,
+			title: "Foundation",
+			pages: 542,
+			publisherId: gnome.id,
+			authors: {
+				connect: [ asimov ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 4 },
+		update: {},
+		create: {
+			id: 4,
+			title: "Galaxy's Edge: Book 1-2",
+			pages: 0,
+			publisherId: podium.id,
+			authors: {
+				connect: [ anspach, cole ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 5 },
+		update: {},
+		create: {
+			id: 5,
+			title: "Pride and Prejudice",
+			pages: 279,
+			publisherId: modern_library.id,
+			authors: {
+				connect: [ austen ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 6 },
+		update: {},
+		create: {
+			id: 6,
+			title: "The Hitchhiker's Guide to the Galaxy",
+			pages: 216,
+			publisherId: del_rey.id,
+			authors: {
+				connect: [ adams ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 7 },
+		update: {},
+		create: {
+			id: 7,
+			title: "The Restaurant at the End of the Universe",
+			pages: 250,
+			publisherId: del_rey.id,
+			authors: {
+				connect: [ adams ],
+			},
+		},
+	});
+
+	await prisma.book.upsert({
+		where: { id: 8 },
+		update: {},
+		create: {
+			id: 8,
+			title: "The Lord of the Rings",
+			pages: 1216,
+			publisherId: allen_and_unwin.id,
+			authors: {
+				connect: [ tolkien ],
+			},
+		},
 	});
 }
 

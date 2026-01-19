b43c91acdbc92f5b589061e3ca62d12cab2810f1
Johan Nordström <johan@digitalvillage.se>
2026-01-19 10:33:56 +0100

    Forgot to add `validateRequest` to Books-router.

--- 04-prisma-books/src/routes/publishers.router.ts
+++ 04-prisma-books/src/routes/publishers.router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { destroy, index, show, store, update } from "../controllers/publisher.controller.ts";
+import { validateRequest } from "../middlewares/validateRequest.ts";
 import { createPublisherRules, updatePublisherRules } from "../rules/publisher.rules.ts";
 
 // Create a Publisher router
@@ -24,14 +25,14 @@ publishersRouter.get("/:publisherId", show);
  *
  * Create an publisher
  */
-publishersRouter.post("/", createPublisherRules, store);
+publishersRouter.post("/", createPublisherRules, validateRequest, store);
 
 /**
  * PATCH /publishers/:publisherId
  *
  * Update a single publisher
  */
-publishersRouter.patch("/:publisherId", updatePublisherRules, update);
+publishersRouter.patch("/:publisherId", updatePublisherRules, validateRequest, update);
 
 /**
  * DELETE /publishers/:publisherId

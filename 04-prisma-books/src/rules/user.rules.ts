88bdf8010ff3a0f07e0085c1487a65c69b2a1e03
Johan Nordström <johan@digitalvillage.se>
2026-01-19 13:21:20 +0100

    Added validation-rules for updating a `User`.

--- 04-prisma-books/src/rules/user.rules.ts
+++ 04-prisma-books/src/rules/user.rules.ts
@@ -4,6 +4,8 @@
 import { body } from "express-validator";
 import { getUserByEmail } from "../services/user.service.ts";
 
+// TODO: Replace duplicate custom validator with a function
+
 export const createUserRules = [
 	body("name")
 		.isString().withMessage("has to be a string").bail()
@@ -25,6 +27,34 @@ export const createUserRules = [
 		}),
 
 	body("password")
+		.isString().withMessage("has to be a string").bail()
+		.isLength({ min: 6 }).withMessage("has be at least 6 characters long"),
+];
+
+export const updateUserRules = [
+	body("name")
+		.optional()
+		.isString().withMessage("has to be a string").bail()
+		.trim()
+		.isLength({ min: 3, max: 191 }).withMessage("has be 3-191 characters long"),
+
+	body("email")
+		.optional()
+		.trim()
+		.isEmail().withMessage("has to be a valid email (duh)").bail()
+		.custom(async (value: string) => {
+			// Check if email exists in the database
+			const user = await getUserByEmail(value);
+
+			// If a user with that email was found, throw an error
+			if (user) {
+				// return Promise.reject("Email already exists");
+				throw new Error("Email already exists");
+			}
+		}),
+
+	body("password")
+		.optional()
 		.isString().withMessage("has to be a string").bail()
 		.isLength({ min: 6 }).withMessage("has be at least 6 characters long"),
 ];

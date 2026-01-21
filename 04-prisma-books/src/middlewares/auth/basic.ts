ef93e1fced9d0b288c60f9eb6267131ef91bece9
Johan Nordström <johan@digitalvillage.se>
2026-01-21 11:15:28 +0100

    [wip] HTTP Basic middleware, step 1-3.

--- 04-prisma-books/src/middlewares/auth/basic.ts
+++ 04-prisma-books/src/middlewares/auth/basic.ts
@@ -11,12 +11,28 @@ export const basic = async (req: Request, res: Response, next: NextFunction) => 
 	debug("Hello from auth/basic! 🙋🏽");
 
 	// 1. Make sure Authorization header exists, otherwise bail 🛑
+	if (!req.headers.authorization) {
+		debug("Authorization header missing");
+		res.status(401).send({ status: "fail", data: { message: "Authorization required but missing" }});
+		return;
+	}
 
 	// 2. Split Authorization header on ` `
+	// "Basic am9oYW5AZGlnaXRhbHZpbGxhZ2Uuc2U6YXBwYXBw"
+	// [0] => "Basic"
+	// [1] => "am9oYW5AZGlnaXRhbHZpbGxhZ2Uuc2U6YXBwYXBw"
+	debug("Authorization header: %o", req.headers.authorization);
+	const [authScheme, base64Payload] = req.headers.authorization.split(" ");
 
 	// 3. Check that Authorization scheme is "Basic", otherwise bail 🛑
+	if (authScheme.toLowerCase() !== "basic") {
+		debug("Authorization Scheme isn't Basic: '%s'", authScheme);
+		res.status(401).send({ status: "fail", data: { message: "Authorization Scheme must be Basic" }});
+		return;
+	}
 
 	// 4. Decode credentials from base64 => ascii
+	debug("base64Payload:", base64Payload);
 
 	// 5. Split credentials on `:`
 

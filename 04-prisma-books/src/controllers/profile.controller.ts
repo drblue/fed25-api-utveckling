daedff1627cf4c80b07fb2a2d126a2b51119f0c6
Johan Nordström <johan@digitalvillage.se>
2026-01-21 12:33:53 +0100

    Send the user's details back to them on `GET /profile`.

--- 04-prisma-books/src/controllers/profile.controller.ts
+++ 04-prisma-books/src/controllers/profile.controller.ts
@@ -2,14 +2,22 @@
  * Profile Controller
  */
 import { Request, Response } from "express";
-import { handlePrismaError } from "../lib/handlePrismaError.ts";
-import { prisma } from "../lib/prisma.ts";
 
 /**
  * Get the authenticated user's profile
  */
-export const getProfile = async (_req: Request, res: Response) => {
-	res.send({ status: "success", data: null });
+export const getProfile = async (req: Request, res: Response) => {
+	// If someone ever removes the authentication middleware from the route for this method, yell at them 😱
+	if (!req.user) {
+		throw new Error("Trying to access authenticated user but none exists. Did you remove authentication from this route? 🤬🤬🤬");
+	}
+
+	// Respond with User 🪪
+	res.send({ status: "success", data: {
+		id: req.user.id,
+		name: req.user.name,
+		email: req.user.email,
+	}});
 }
 
 /**

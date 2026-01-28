157e817b673ce1a8f7204cc464f1f479907a892f
Johan Nordström <johan@digitalvillage.se>
2026-01-28 12:51:33 +0100

    Make sure a movie runtime is a positive number and that release_year isn't before 1888 or in the future.

--- 05-lmdb/src/resources/movie/movie.model.ts
+++ 05-lmdb/src/resources/movie/movie.model.ts
@@ -6,6 +6,8 @@ export interface MovieDocument extends Document {
 	release_year?: number;
 }
 
+const currentYear = new Date().getFullYear();
+
 const movieSchema = new Schema<MovieDocument>({
 	title: {
 		type: String,
@@ -13,9 +15,12 @@ const movieSchema = new Schema<MovieDocument>({
 	},
 	runtime: {
 		type: Number,
+		min: [1, "has to be a positive number"],
 	},
 	release_year: {
 		type: Number,
+		min: [1888, "has to be 1888 or later"],
+		max: [currentYear, "cannot be in the future"],
 	},
 });
 

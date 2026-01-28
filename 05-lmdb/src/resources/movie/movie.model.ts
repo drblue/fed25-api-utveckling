0422e3a775e88810675461c75c3a4fdd28d6e09a
Johan Nordström <johan@digitalvillage.se>
2026-01-28 12:55:18 +0100

    Sanitize title when creating a movie. Also default to null for runtime and release_year if they're not set.

--- 05-lmdb/src/resources/movie/movie.model.ts
+++ 05-lmdb/src/resources/movie/movie.model.ts
@@ -2,8 +2,8 @@ import { Document, model, Schema } from "mongoose";
 
 export interface MovieDocument extends Document {
 	title: string;
-	runtime?: number;
-	release_year?: number;
+	runtime: number | null;  // `?` can be removed when we default the schema-field to null
+	release_year: number | null;
 }
 
 const currentYear = new Date().getFullYear();
@@ -12,13 +12,17 @@ const movieSchema = new Schema<MovieDocument>({
 	title: {
 		type: String,
 		required: true,
+		trim: true,
+		minlength: [3, "has to be at least 3 characters"],
 	},
 	runtime: {
 		type: Number,
+		default: null,
 		min: [1, "has to be a positive number"],
 	},
 	release_year: {
 		type: Number,
+		default: null,
 		min: [1888, "has to be 1888 or later"],
 		max: [currentYear, "cannot be in the future"],
 	},

b979d51eae7940261c671b98a709eea3047004cc
Johan Nordström <johan@digitalvillage.se>
2026-02-05 11:10:13 +0100

    Added `Room` seeder. Don't forget to run `npx prisma db seed`.

--- 06-chat/backend/prisma/seed.ts
+++ 06-chat/backend/prisma/seed.ts
@@ -3,6 +3,26 @@ import { prisma } from "../src/lib/prisma.ts";
 const main = async () => {
 	// 🌱 Here be all your seeds 🍼👶🏻...
 
+	/**
+	 * Create all the neccessary rooms
+	 */
+	await prisma.room.upsert({
+		where: { name: "General" },
+		update: {},
+		create: { name: "General" },
+	});
+
+	await prisma.room.upsert({
+		where: { name: "Major" },
+		update: {},
+		create: { name: "Major" },
+	});
+
+	await prisma.room.upsert({
+		where: { name: "Private" },
+		update: {},
+		create: { name: "Private" },
+	});
 }
 
 main()
